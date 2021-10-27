c956641d196055a04244b8620396568c7eb107ed
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2021-10-27 13:41:35 +0100

    Add SAP_Adapter project

---
+++ SAP_Adapter/AdapterActions/Execute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BH.oM.Reflection;
+using BH.oM.Adapter;
+using BH.oM.Environment.SAP;
+
+namespace BH.Adapter.SAP
+{
+    public partial class SAPAdapter
+    {
+        public override Output<List<object>, bool> Execute(IExecuteCommand command, ActionConfig actionConfig = null)
+        {
+            var output = new Output<List<object>, bool>() { Item1 = null, Item2 = false };
+
+            output.Item2 = RunCommand(command as dynamic);
+
+            return output;
+        }
+
+        public bool RunCommand(RunAnalysisCommand command)
+        {
+            return true;
+        }
+    }
+}
---
+++ SAP_oM/RunAnalysisCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Adapter;
+using BH.oM.Base;
+
+namespace BH.oM.Environment.SAP
+{
+    public class RunAnalysisCommand : IExecuteCommand, IObject
+    {
+
+    }
+}
