41d6fbcbc8e7ccc04b8c6092658cc966be85d436
Ross Boulton <ross.boulton@burohappold.com>
2021-11-02 18:27:57 +0000

    Update order of Opening and Wall object properties

--- SAP_oM/Wall.cs
+++ SAP_oM/Wall.cs
@@ -31,9 +31,6 @@ namespace BH.oM.Environment.SAP
     [Description("A wall that forms part of the thermal line of the dwelling")]
     public class Wall : BHoMObject
     {
-        [Description("A description of the wall.")]
-        public virtual string Description { get; set; } = "Wall";
-
         [Description("The total surface area of the wall as seen from inside the dwelling")]
         public virtual double Area { get; set; } = 0;
 
