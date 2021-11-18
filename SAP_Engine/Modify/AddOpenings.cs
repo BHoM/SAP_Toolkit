0e61b8a9049b7b14b5ccb4dff2de1bedf111e5b3
Milo Gillot <milo.gillot@burohappold.com>
2021-11-18 15:47:40 +0000

    Changes to AddOpenings method

--- SAP_Engine/Modify/AddOpenings.cs
+++ SAP_Engine/Modify/AddOpenings.cs
@@ -20,182 +20,73 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System;
+//using BH.Engine.Base;
+//using BH.oM.Base;
+//using BH.oM.XML.Bluebeam;
+//using BH.oM.Environment.Elements;
+//using BH.oM.Geometry;
+//using BH.Engine.Geometry;
+//using BH.Engine.Units;
+//using BH.oM.Geometry.SettingOut;
+//using BH.oM.Analytical.Elements;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel;
+//using BH.oM.Reflection.Attributes;
+//using System;
+//using System.Text;
+//using System.Threading.Tasks;
+
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using BH.oM.Reflection.Attributes;
 using BH.oM.Environment.SAP;
-using BH.oM.Environment.Elements;
-using BH.oM.Geometry;
-using BH.Engine.Geometry;
-using BH.Engine.Units;
-using BH.oM.Geometry.SettingOut;
-using BH.oM.Analytical.Elements;
 using BH.oM.Reflection;
-using BH.Engine.Base;
-using BH.oM.Base;
-using BH.oM.XML.Bluebeam;
-
 using Opening = BH.oM.Environment.SAP.Opening;
 
 namespace BH.Engine.Environment.SAP
 {
     public static partial class Modify
     {
-        public static Output<Wall, List<Wall>> AddOpenings(this List<Markup> markups, List<Opening> openings)
+        public static Output<List<Wall>, bool> AddOpenings(this List<Wall> walls, List<Opening> openings)
         {
-            List<double> areas = new List<double>();
-            foreach (Markup markup in markups)
+            double totalOpeningAreas = (double)openings.Sum(area => area.Area);
+            bool alert = true;
+            int j = 0;
+            double areaCounter = 0;
+
+            List<Opening> sortedOpenings = Enumerable.Reverse(openings.OrderBy(x1 => x1.Area)).ToList();
+            List<Wall> sortedWalls = Enumerable.Reverse(walls.OrderBy(x1 => x1.Area)).ToList();
+            List<Opening> groupedOpenings = new List<Opening>();
+            List<Wall> allWalls = new List<Wall>();
+
+            //Alert when total area of the openings exceeds the area of the largest wall within same space.
+            if (sortedWalls[0].Area < totalOpeningAreas)
             {
-                areas.Add(markup.Length * markup.Depth); 
+                alert = false;
             }
 
-            
-            int indexAtMax = areas.IndexOf(areas.Max());
-            List<Wall> wallsWithoutOpenings = new List<Wall>();
-
-            for (int i = 0; i < markups.Count; i++)
+            for (int i = 0; i < sortedWalls.Count; i++)
             {
-                if (i != indexAtMax)
+                allWalls.Add(sortedWalls[i]);
+
+                while ((areaCounter < sortedWalls[i].Area) && (j < sortedOpenings.Count))
                 {
-                    Wall temp = new Wall();
-                    temp.Area = markups[i].Length * markups[i].Depth;
-                    temp.DwellingName = "dwelling";
-                    temp.Name = markups[i].Label;
-                    wallsWithoutOpenings.Add(temp);
-
+                    areaCounter += sortedOpenings[j].Area;
+                    groupedOpenings.Add(sortedOpenings[j]);
+                    j += 1;
                 }
+                allWalls[i].Openings = groupedOpenings;
+                areaCounter = 0;
+                groupedOpenings.Clear();
             }
-            Markup x = markups[indexAtMax];
 
-            Wall y = new Wall();
-            y.Openings = openings;
-            y.Area = areas.Max();
-            y.DwellingName = "dwelling";
-            y.Name = x.Label;
-
-
-            return new Output<Wall, List<Wall>>
+            return new Output<List<Wall>, bool>()
             {
-                Item1 = y,
-                Item2 = wallsWithoutOpenings,
+                Item1 = allWalls,
+                Item2 = alert
             };
+
         }
-        
+
     }
 }
-
-//var listA = walls;
-//var listB = wallAreas;
-
-
-//var items = new List<Tuple<List<CustomObject>, List<double>>>();
-//for (var i = 0; i < listA.Count; ++i)
-//    items.Add(Tuple.Create(listA[i], listB[i]));
-
-//var sorted = items.OrderBy(x => x.Item1);
-
-//listA = sorted.Select(x => x.Item1).ToList();
-//listB = sorted.Select(x => x.Item2).ToList();
-
-
-// <Custom additional code> 
-
-
-/*foreach (Polyline baseCurve in baseCurves)
-            {
-                double minBaseLevel = baseCurve.MinimumLevel();
-                List<Space> spacesOnLevel = spaces.Where(x => x.Perimeter.ICollapseToPolyline(angleTolerance).MinimumLevel() == minBaseLevel).ToList(); // Spaces on the same level as the basecurve
-                if (spacesOnLevel.Count == 0)
-                    continue;
-
-                List<Space> newSpaces = resnappedSpaces.Where(x => x.Perimeter.ICollapseToPolyline(angleTolerance).MinimumLevel() == minBaseLevel).ToList(); // Spaces on the same level as the basecurve
-
-                List<Point> corners = baseCurve.ControlPoints();
-                List<Polyline> basecurveSegments = baseCurve.SplitAtPoints(corners);
-                List<Point> invertedPoints = new List<Point>();
-                foreach (Point pt in corners)
-                {
-                    List<Polyline> cornerLines = basecurveSegments.Where(x => pt.IIsOnCurve(x)).ToList();
-                    if (cornerLines.Count() > 1)
-                    {
-                        Point centre1 = cornerLines[0].Centre();
-                        Point centre2 = cornerLines[1].Centre();
-                        Line joined = new Line() { Start = centre1, End = centre2 };
-
-                        if (!(baseCurve.IIsContaining(new List<Point>() { joined.Centroid() })))
-                            invertedPoints.Add(pt);
-                    }
-                }
-
-                Space prioritySpace = new Space();
-                foreach (Point pt in invertedPoints)
-                {
-                    List<Space> connectedSpaces = spacesOnLevel.Where(x => pt.IIsOnCurve(x.Perimeter)).ToList();
-                    foreach (Space space in connectedSpaces)
-                    {
-                        List<Polyline> spaceParts = space.Perimeter.ICollapseToPolyline(BH.oM.Geometry.Tolerance.Distance).SplitAtPoints(space.Perimeter.IControlPoints()).ToList();
-                        List<Polyline> parts = spaceParts.Where(x => pt.IIsOnCurve(x)).ToList();
-                        if (parts.Count > 1)
-                        {
-                            Point centre1 = parts[0].Centre();
-                            Point centre2 = parts[1].Centre();
-                            Line joined = new Line() { Start = centre1, End = centre2 };
-                            if (!((joined.Centroid().IIsOnCurve(parts[0]) || joined.Centroid().IIsOnCurve(parts[1]))))
-                                prioritySpace = space;
-                        }
-                    }
-
-                    List<Space> priorityResnapped = newSpaces.Where(x => x.Name == prioritySpace.Name).ToList();
-                    int nr = connectedSpaces.IndexOf(prioritySpace);
-                    connectedSpaces.RemoveAt(nr);
-                    List<Space> resnappedConnectedspaces = new List<Space>();
-                    foreach (Space space in connectedSpaces)
-                        resnappedConnectedspaces.Add(newSpaces.Where(x => x.Name == space.Name).FirstOrDefault());
-
-                    List<Polyline> resnappedPerimeters = priorityResnapped.Select(x => x.Perimeter.ICollapseToPolyline(BH.oM.Geometry.Tolerance.Angle)).ToList();
-
-                    foreach (Space space in resnappedConnectedspaces)
-                    {
-                        Polyline perimeter = space.Perimeter.ICollapseToPolyline(BH.oM.Geometry.Tolerance.Angle);
-                        Polyline newPerimeter = perimeter.BooleanDifference(resnappedPerimeters).FirstOrDefault();
-                        space.Perimeter = newPerimeter;
-                        Space matchingSpace = newSpaces.Where(x => x.Name == space.Name).FirstOrDefault();
-                        int number = resnappedSpaces.IndexOf(matchingSpace);
-                        resnappedSpaces[number] = space;
-
-                        List<Panel> panels1 = panels.ToSpace(space.Name);
-                        double spaceLevel = space.Perimeter.ICollapseToPolyline(angleTolerance).MinimumLevel();
-                        Panel floor = panels1.Where(x => x.MinimumLevel() == spaceLevel).FirstOrDefault();
-                        Panel ceiling = panels1.Where(x => x.MinimumLevel() != spaceLevel).FirstOrDefault();
-
-                        if (floor != null)
-                        {
-                            int floorIndex = panels.IndexOf(floor);
-                            floor.ExternalEdges = space.Perimeter.ToEdges();
-
-                            if (floorIndex != -1)
-                                panels[floorIndex] = floor;
-                        }
-
-                        if (ceiling != null)
-                        {
-                            int ceilingIndex = panels.IndexOf(ceiling);
-                            double z = ceiling.MinimumLevel();
-                            Polyline spaceClone = space.Perimeter.ICollapseToPolyline(angleTolerance).DeepClone();
-                            spaceClone.ControlPoints = spaceClone.IControlPoints().Select(y => new Point { X = y.X, Y = y.Y, Z = z }).ToList();
-                            ceiling.ExternalEdges = space.Perimeter.ToEdges();
-
-                            if (ceilingIndex != -1)
-                                panels[ceilingIndex] = ceiling;
-                        }
-                    }
-                }
-            }
-*/
