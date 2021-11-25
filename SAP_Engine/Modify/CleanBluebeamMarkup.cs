cae71407a7d7392903d4a21ee47e3fdee4355089
Emil Svedjer <emil.svedjer@burohappold.com>
2021-11-25 14:15:35 +0000

    added input per Fraser's suggestion

--- SAP_Engine/Modify/CleanBluebeamMarkup.cs
+++ SAP_Engine/Modify/CleanBluebeamMarkup.cs
@@ -12,26 +12,41 @@ using System.ComponentModel;
 using BH.Engine.Base;
 using System.IO;
 
+
 namespace BH.Engine.Environment.SAP
 {
     public static partial class Modify
     {
         [Description("Cleaning Bluebeam XML export.")]
-        [Input("inputDirectory", "Full directory to the file.")]
+        [Input("fileSettings", "Full directory to the file.")]
+        [Input("fileSettingsOutut", "Full directory for the output file.")]
         [Input("run", "Toggle to acitvate the component.")]
         [Output("outputDirectory","Directory to the new file.")]
-        public static string CleanBluebeamMarkup(string inputDirectory, bool run = false)
+        public static string CleanBluebeamMarkup(BH.oM.Adapter.FileSettings fileSettings = null, BH.oM.Adapter.FileSettings fileSettingsOutput = null, bool run = false)
         {
             if (!run)
                 return null;
 
-            if (!File.Exists(inputDirectory))
+            if (fileSettings == null || fileSettingsOutput == null)
+            {
+                BH.Engine.Reflection.Compute.RecordError("Please set the File Settings correctly to enable the component to read the correct file.");
+                return null;
+            }
+
+            if (!Path.HasExtension(fileSettings.FileName) || (Path.GetExtension(fileSettings.FileName) != ".xml" && Path.GetExtension(fileSettings.FileName) != ".csproj")
+                || !Path.HasExtension(fileSettingsOutput.FileName) || (Path.GetExtension(fileSettingsOutput.FileName) != ".xml" && Path.GetExtension(fileSettingsOutput.FileName) != ".csproj"))
+            {
+                BH.Engine.Reflection.Compute.RecordError("File name must contain a file extension.");
+                return null;
+            }
+            string id = @"" + fileSettings.Directory + "\\" + fileSettings.FileName;
+
+            if (!File.Exists(id))
             {
                 BH.Engine.Reflection.Compute.RecordError("Input directory contains a null value or does not exist. Please check your inputs.");
                 return null;
             }
-            string id = @"" + inputDirectory;
-            string outputDirectory = id.Replace(".xml", "_cleaned.xml");
+            string outputDirectory = fileSettingsOutput.Directory + "\\" + fileSettingsOutput.FileName;
             File.WriteAllLines(outputDirectory, File.ReadAllLines(id).Where(line => !(line.Trim().StartsWith("<") && line.Trim().Contains("/>"))));
             return outputDirectory;
         }
