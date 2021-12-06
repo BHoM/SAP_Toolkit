0c1dc03e811452658a3457be5bc5be9afa79c31f
Ross Boulton <ross.boulton@burohappold.com>
2021-12-06 16:43:50 +0000

    Add user input for postURL

--- SAP_oM/RunAnalysisCommand.cs
+++ SAP_oM/RunAnalysisCommand.cs
@@ -12,7 +12,7 @@ namespace BH.oM.Environment.SAP
     public class RunAnalysisCommand : IExecuteCommand, IObject
     {
         public virtual string InputFile { get; set; }
-        //public virtual string PostURL { get; set; } = "https://ace.argylesoftware.co.uk/buroh/v2/sap-fullsapworksheet-lig-in-text-out";
         public virtual string APIKey { get; set; }
+        public virtual string PostURL { get; set; }
     }
 }
