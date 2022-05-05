0d9c019945eae2b6808e127d03c46ae731d93015
Emil Svedjer <emil.svedjer@burohappold.com>
2022-05-05 10:10:47 +0100

    Changes to BuildingPart convert + added new SAP object

--- SAP_Engine/Convert/BuildingPart.cs
+++ SAP_Engine/Convert/BuildingPart.cs
@@ -33,30 +33,29 @@ namespace BH.Engine.Environment.SAP
 {
     public static partial class Convert
     {
-        [Description("Convert lists of SAP Objects to a XML BuildingPart.")]
-        [Input("buildingPartNumber", ">An integer value which uniquely identifies the building part in the property.  The value 1 must be assigned to the main dwelling.")]
-        [Input("identifier", "Identifier for the Building part - generally only required if there are more that one Building Parts of the same type e.g. West Wing and East Wing Extensions.")]
-        [Input("constructionYear", "The year when this building part was constructed.")]
-        [Input("sapOpenings", "List of SAP openings to convert.")]
-        [Input("sapFloors", "List of SAP floors to convert.")]
-        [Input("sapRoofs", "List of SAP roofs to convert.")]
-        [Input("sapWalls", "List of SAP walls to convert.")]
-        [Input("sapThermalBridges", "List of SAP thermal bridges to convert.")]
-        [Output("xmlBuildingPart", "XML BuildingPart.")]
-        public static BH.oM.Environment.SAP.XML.BuildingPart ToXML(string buildingPartNumber = "1", string identifier = "Main dwelling", string constructionYear = "2021", BH.oM.Environment.SAP.WindowOvershading overshading = BH.oM.Environment.SAP.WindowOvershading.AverageOrUnknown, List<BH.oM.Environment.SAP.Opening> sapOpenings = null, List<BH.oM.Environment.SAP.Floor> sapFloors = null, List<BH.oM.Environment.SAP.Roof> sapRoofs = null, List<BH.oM.Environment.SAP.Wall> sapWalls = null, List<BH.oM.Environment.SAP.ThermalBridge> sapThermalBridges= null)
+        [Description("Convert lists of SAP BuildingParts to a XML BuildingParts.")]
+        [Input("sapBuldingPart","SAP Building parts to convert.")]
+        [Output("xmlBuildingParts", "XML BuildingParts.")]
+        public static BH.oM.Environment.SAP.XML.BuildingParts ToXML(List<oM.Environment.SAP.BuildingPart> sapBuildingPart)
         {
-            BH.oM.Environment.SAP.XML.BuildingPart xmlBuildingPart = new BH.oM.Environment.SAP.XML.BuildingPart();
-            xmlBuildingPart.BuildingPartNumber = buildingPartNumber; 
-            xmlBuildingPart.Identifier = identifier;
-            xmlBuildingPart.ConstructionYear = constructionYear;
-            xmlBuildingPart.Overshading = overshading.FromSAPToXML();
-            xmlBuildingPart.Openings = sapOpenings.Select(x => x.ToXML()).ToList();
-            xmlBuildingPart.Floors = sapFloors.Select(x => x.ToXML()).ToList();
-            xmlBuildingPart.Roofs = sapRoofs.Select(x => x.ToXML()).ToList();
-            xmlBuildingPart.Walls = sapWalls.Select(x => x.ToXML()).ToList();
-            xmlBuildingPart.ThermalBridges = sapThermalBridges.Select(x => x.ToXML()).ToList();
-
-            return xmlBuildingPart;
+            List<BH.oM.Environment.SAP.XML.BuildingPart> xmlBuildingParts = new List<BH.oM.Environment.SAP.XML.BuildingPart>();
+            for (int i = 0; i < sapBuildingPart.Count; i++)
+            {
+                BH.oM.Environment.SAP.XML.BuildingPart xmlBuildingPart = new BH.oM.Environment.SAP.XML.BuildingPart();
+                xmlBuildingPart.BuildingPartNumber = sapBuildingPart[i].buildingPartNumber;
+                xmlBuildingPart.Identifier = sapBuildingPart[i].identifier;
+                xmlBuildingPart.ConstructionYear = sapBuildingPart[i].constructionYear;
+                xmlBuildingPart.Overshading = sapBuildingPart[i].Overshading.FromSAPToXML();
+                xmlBuildingPart.Openings = sapBuildingPart[i].Openings.Select(x => x.ToXML()).ToList();
+                xmlBuildingPart.Floors = sapBuildingPart[i].Floors.Select(x => x.ToXML()).ToList();
+                xmlBuildingPart.Roofs = sapBuildingPart[i].Roofs.Select(x => x.ToXML()).ToList();
+                xmlBuildingPart.Walls = sapBuildingPart[i].Walls.Select(x => x.ToXML()).ToList();
+                xmlBuildingPart.ThermalBridges = sapBuildingPart[i].ThermalBridges.Select(x => x.ToXML()).ToList();
+                xmlBuildingParts.Add(xmlBuildingPart);
+            }
+            oM.Environment.SAP.XML.BuildingParts finalxml = new oM.Environment.SAP.XML.BuildingParts();
+            finalxml.BuildingPart = xmlBuildingParts;
+            return finalxml;
         }
         private static string FromSAPToXML(this BH.oM.Environment.SAP.WindowOvershading windowOvershading)
         {
