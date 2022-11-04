cc07947352698c38f63e181ac392130f129f1f1d
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-11-04 15:45:41 +0000

    Initial convert methods for demo

---
+++ SAP_Engine/Compute/QuickTestForStroma.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using BH.oM.Environment.SAP.Stroma10;
+using BH.oM.Base;
+
+namespace BH.Engine.Environment.SAP
+{
+    public static partial class Compute
+    {
+        public static Root ConvertFileToRoot(string filePath)
+        {
+            StreamReader sr = new StreamReader(filePath);
+            string line = sr.ReadToEnd();
+            sr.Close();
+
+            CustomObject obj = BH.Engine.Serialiser.Convert.FromJson(line)as CustomObject;
+
+            return BH.Engine.Environment.SAP.Stroma10.Convert.ToRoot(obj);
+        }
+    }
+}
