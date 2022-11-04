46dc92b2682d892d9c998639122332e72b6eb399
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2022-11-04 15:45:41 +0000

    Initial convert methods for demo

---
+++ SAP_Engine/Convert/Stroma10Json/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BH.oM.Base;
+
+namespace BH.Engine.Environment.SAP.Stroma10
+{
+    public static partial class Convert
+    {
+        public static BH.oM.Environment.SAP.Stroma10.Address ToAddress(CustomObject addressObject)
+        {
+            BH.oM.Environment.SAP.Stroma10.Address sapAddress = new BH.oM.Environment.SAP.Stroma10.Address();
+
+            sapAddress.Postcode = addressObject.CustomData["Postcode"] as string;
+
+            return sapAddress;
+        }
+    }
+}
---
+++ SAP_Engine/Convert/Stroma10Json/Root.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using BH.oM.Environment.SAP.Stroma10;
+using BH.oM.Base;
+
+namespace BH.Engine.Environment.SAP.Stroma10
+{
+    public static partial class Convert
+    {
+        public static Root ToRoot(CustomObject rootObject)
+        {
+            Root sapRoot = new Root();
+
+            sapRoot.Id = System.Convert.ToInt32(rootObject.CustomData["Id"]);
+
+            sapRoot.Address = ToAddress(rootObject.CustomData["Address"] as CustomObject);
+
+            return sapRoot;
+        }
+    }
+}
