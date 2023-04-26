910f2fa6c7f8432e1b2f035b6d6506bfb2108511
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-04-26 14:13:19 +0100

    Add CI folder

---
+++ .ci/unit-tests/SAP_Toolkit_Tests/SAP_Toolkit_Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SAP_Toolkit_Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
