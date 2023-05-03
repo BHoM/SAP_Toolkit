4186ad203af22cdaa0dfbd2e4a27d859ae328e9b
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-05-03 15:27:33 +0100

    Add Bluebeam SAP objects

---
+++ SAP_oM/Bluebeam/BluebeamSAPObject.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace BH.oM.Environment.SAP.Bluebeam
+{
+    [XmlInclude(typeof(SAPMarkupSummary))]
+    [XmlInclude(typeof(SAPMarkup))]
+    public class BluebeamSAPObject : IBHoMObject
+    {
+        [XmlIgnore]
+        public virtual Guid BHoM_Guid { get; set; } = Guid.NewGuid();
+        [XmlIgnore]
+        public virtual Dictionary<string, object> CustomData { get; set; }
+        [XmlIgnore]
+        public virtual string Name { get; set; }
+        [XmlIgnore]
+        public virtual FragmentSet Fragments { get; set; }
+        [XmlIgnore]
+        public virtual HashSet<string> Tags { get; set; }
+    }
+}
