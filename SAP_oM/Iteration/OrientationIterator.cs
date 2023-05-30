5f49da99def7bbd9cd57e7200aee83256c1a73a0
Ellie Adebowale <ellie.adebowale@burohappold.com>
2023-05-30 15:47:29 +0100

    Creation of objects to be used to set up a parametric study.

---
+++ SAP_oM/Iteration/OrientationIterator.cs
@@ -0,0 +1,51 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using BH.oM.Environment.SAP.XML;
+using BH.oM.Environment.SAP;
+using System.ComponentModel;
+using BH.oM.Base;
+using BH.oM.Environment.SAP.Stroma10;
+
+namespace BH.oM.Environment.SAP.XML
+
+{
+    [Description("Input changes to make to the orientation of the dwelling.")]
+    public class OrientationIterator : IObject
+    {
+        [Description("Rotate dwelling clockwise: 1 - 8 scale.")]
+        public virtual string Rotation { get; set; } = null;
+
+        [Description("Mirror the dwelling.")]
+        public virtual Mirror Mirror { get; set; } = Mirror.None;
+
+    }
+}
+
+
---
+++ SAP_oM/Iteration/Parameters.cs
@@ -0,0 +1,69 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using BH.oM.Environment.SAP.XML;
+using BH.oM.Environment.SAP;
+using System.ComponentModel;
+using BH.oM.Base;
+using BH.oM.Environment.SAP.Stroma10;
+
+namespace BH.oM.Environment.SAP.XML
+
+{
+    [Description(".")]
+    public class Parameters : IObject
+    {
+        [Description("CurrentIterator name.")]
+        public virtual string IterationName { get; set; } = null;
+
+        [Description("List of opening type iterators.")]
+        public virtual List<OpeningTypeIterator> OpeningTypes { get; set; } = null;
+
+        [Description("List of opening iterators.")]
+        public virtual List<OpeningIterator> Openings { get; set; } = null;
+
+        [Description("List of wall iterators.")]
+        public virtual List<WallIterator> Walls { get; set; } = null;
+
+        [Description("Orientation Iterator")]
+        public virtual OrientationIterator Orientation { get; set; } = null;
+
+        [Description("List of roof iterators.")]
+        public virtual List<RoofIterator> Roofs { get; set; } = null;
+
+        [Description("List of floor iterators.")]
+        public virtual List<FloorIterator> Floors { get; set; } = null;
+
+        [Description("TB to change psi values - iterator")]
+        public virtual List<ThermalBridgeIterator> ThermalBridges { get; set; } = null;
+
+    }
+}
+
+
