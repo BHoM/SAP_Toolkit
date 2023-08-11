3e1faf8f25f48a9b3f568446d20d379951fa0d6a
Ellie Adebowale <ellie.adebowale@burohappold.com>
2023-08-11 11:46:26 +0100

    Update of the roof modification methods.

--- SAP_Engine/Modify/Rotate.cs
+++ SAP_Engine/Modify/Rotate.cs
@@ -50,98 +50,138 @@ namespace BH.Engine.Environment.SAP
         [Output("sapReport", "The mirrored report.")]
         public static SAPReport RotateDwelling(this SAPReport sapObj, string rotation)
         {
-            //Nothing inputted
+            //Null sapObj input
             if (sapObj == null)
             {
                 BH.Engine.Base.Compute.RecordError("No sap object inputted to mirror.");
                 return null;
             }
 
+            if (rotation == null)
+            {
+                return sapObj;
+            }
+
             BH.oM.Environment.SAP.XML.PropertyDetails propertyDetailsObj = sapObj.SAP10Data.PropertyDetails;
-            //Rotate the orientation of the dwelling
-            propertyDetailsObj.Orientation = propertyDetailsObj.Orientation.RotateOrientation(rotation);
 
-            //Try to rotate the PV in EnergySource
-            try
+            //Rotate the orientation of the dwelling if the orientation of the dwelling is not null
+            if (propertyDetailsObj.Orientation != null)
             {
+                string orientation = propertyDetailsObj.Orientation.RotateOrientation(rotation);
+
+                propertyDetailsObj.Orientation = orientation;
+            }
+            else
+            {
+                BH.Engine.Base.Compute.RecordError("Please input an orientation for the dwelling.");
+            }
+
+            //Rotate the PV in EnergySource
+            if (propertyDetailsObj.EnergySource != null && propertyDetailsObj.EnergySource.PhotovoltaicArrays != null && propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray != null)
+            {
+                //List of existing PVArrays in Energy source
                 List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> PVArrays = propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray;
-                foreach (var array in PVArrays)
-                {
-                    if (array != null)
-                    {
-                        if (array.Orientation == "0" || array.Orientation == "9")
-                        {
-                            BH.Engine.Base.Compute.RecordWarning("PV is horizontal and therefore can't be rotated");
-                            continue;
-                        }
-                        array.Orientation = array.Orientation.RotateOrientation(rotation);
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-                propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray = PVArrays;
-            }
-            catch (Exception e)
-            {
-                BH.Engine.Base.Compute.RecordWarning("If you have no PV ignore this warning, otherwise:" + e.Message);
+
+                //Assign the list of modified arrays to the property details obj
+                propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray = PVArrays.RotatePV(rotation);
             }
 
-            //Try to rotate the PV in MainHeating
-            try
+            //Rotate the PV in MainHeating
+            //Checking if Mainheating object is defined and if it isn't: side eye
+            if (propertyDetailsObj.Heating != null && propertyDetailsObj.Heating.MainHeatingDetails != null && propertyDetailsObj.Heating.MainHeatingDetails.MainHeating != null)
             {
+                //List of existing heating systems in report
                 List<MainHeating> heatingObjs = propertyDetailsObj.Heating.MainHeatingDetails.MainHeating;
+
+                //For each heating system
                 foreach (var heating in heatingObjs)
                 {
-                    List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> FGHRSPVArrays = heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray;
-                    foreach (var array in FGHRSPVArrays)
+                    //Check if pv is defined in the report
+                    if (heating.FGHRSEnergySource != null && heating.FGHRSEnergySource.PhotovoltaicArrays != null && heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray != null)
                     {
-                        if (array != null)
-                        {
-                            if (array.Orientation == "0" || array.Orientation == "9")
-                            {
-                                BH.Engine.Base.Compute.RecordWarning("PV is horizontal and therefore can't be rotated");
-                                continue;
-                            }
-                            array.Orientation = array.Orientation.RotateOrientation(rotation);
-                        }
-                        else
-                        {
-                            continue;
-                        }
+                        //A list of existing PV arrays in the Main Heating section
+                        List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> fghrsPVArrays = heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray;
+
+                        heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray = fghrsPVArrays.RotatePV(rotation);
                     }
-                    heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray = FGHRSPVArrays;
                 }
                 propertyDetailsObj.Heating.MainHeatingDetails.MainHeating = heatingObjs;
-
-            }
-            catch (Exception e)
-            {
-                BH.Engine.Base.Compute.RecordWarning("If you have no PV ignore this warning, otherwise:" + e.Message);
             }
 
+            sapObj.SAP10Data.PropertyDetails = propertyDetailsObj.RotateOpening(rotation);
+
+            return sapObj;
+        }
+
+        [Description("Modify the orientation of the openings.")]
+        [Input("propertyDetailsObj", "PropertyDetails object to modify the openings within.")]
+        [Input("rotation", "Line to rotate across.")]
+        [Output("propertyDetails", "Modified openings contained in the property details object.")]
+        public static BH.oM.Environment.SAP.XML.PropertyDetails RotateOpening(this BH.oM.Environment.SAP.XML.PropertyDetails propertyDetailsObj, string rotation)
+        {
             //Rotate each opening in the dwelling
             List<BH.oM.Environment.SAP.XML.BuildingPart> buildingPartObj = propertyDetailsObj.BuildingParts.BuildingPart;
+            List<BH.oM.Environment.SAP.XML.OpeningType> types = propertyDetailsObj.OpeningTypes.OpeningType;
+
+            //Dictionary for the conversion between the name of the type given by the user and the name of the opening that matches the schema.
+            Dictionary<string, string> typeMap = types.Select(x => new { Key = x.Name, Value = x.Description }).ToDictionary(x => x.Key, x => x.Value);
+
             foreach (var b in buildingPartObj)
             {
+                //List of existing openings
                 List<BH.oM.Environment.SAP.XML.Opening> openingObjs = b.Openings.Opening;
                 foreach (var o in openingObjs)
                 {
                     if (o.Orientation == "0" || o.Orientation == "9")
                     {
-                        BH.Engine.Base.Compute.RecordWarning("Window is horizontal and therefore can't be rotated");
                         continue;
                     }
-                    o.Orientation = o.Orientation.RotateOrientation(rotation);
+
+                    //Find the new orientation of the opening
+                    string orientation = o.Orientation.RotateOrientation(rotation);
+
+                    //Changing orientation
+                    o.Orientation = orientation;
                 }
                 b.Openings.Opening = openingObjs;
             }
 
             propertyDetailsObj.BuildingParts.BuildingPart = buildingPartObj;
-            sapObj.SAP10Data.PropertyDetails = propertyDetailsObj;
 
-            return sapObj;
+            return propertyDetailsObj;
+        }
+
+
+        [Description("Modify the orientation of the PV.")]
+        [Input("PVArrays", "List of photovoltaic arrays to modify the orientation of.")]
+        [Input("rotateLine", "Line to rotate across.")]
+        [Output("PVarrayObject", "List of modified PV objects.")]
+        public static List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> RotatePV(this List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> PVArrays, string rotation)
+        {
+            //For each pv array
+            foreach (var array in PVArrays)
+            {
+                if (array != null)
+                {
+                    //If array orientation is ND - horizontal or NR not defined
+                    if (array.Orientation == "ND" || array.Orientation == "NR" || array.Orientation == null)
+                    {
+                        array.Orientation = "ND";
+                        continue;
+                    }
+
+                    //Find the rotateed orientation
+                    string orientation = array.Orientation.RotateOrientation(rotation);
+
+                    //Assign new orientation to array
+                    array.Orientation = orientation;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return PVArrays;
         }
 
         [Description("Rotate orientation to new orientation.")]
@@ -150,9 +190,10 @@ namespace BH.Engine.Environment.SAP
         [Output("Orientation", "New orientation.")]
         public static string RotateOrientation(this string orientation, string rotation)
         {
-            if (orientation == null || orientation == "0" || orientation == "9")
+            //IF orientation is null or horizontal or not defined
+            if (orientation == null || orientation == "0" || orientation == "9" || orientation == "ND" || orientation == "NR")
             {
-                return null;
+                return orientation;
             }
 
             if (rotation == null || rotation == "0" || rotation == "9")
