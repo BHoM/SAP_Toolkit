8a48f1724bf9177c24b32fd5912ab4409608ede7
Ellie Adebowale <ellie.adebowale@burohappold.com>
2023-08-11 11:04:51 +0100

    Update of the Mirror modification methods in efforts to make more readable. And addition of comments.

--- SAP_Engine/Modify/Mirror.cs
+++ SAP_Engine/Modify/Mirror.cs
@@ -38,6 +38,9 @@ using BH.oM.Base;
 using BH.oM.Environment.SAP.XML;
 using System.Runtime.InteropServices.ComTypes;
 using BH.oM.Adapter.Commands;
+using System.Linq.Expressions;
+using BH.oM.Environment.SAP.Stroma10;
+using BH.oM.Quantities.Attributes;
 
 namespace BH.Engine.Environment.SAP
 {
@@ -49,101 +52,136 @@ namespace BH.Engine.Environment.SAP
         [Output("sapReport", "The modified SAP Report object.")]
         public static SAPReport MirrorDwelling(this SAPReport sapObj, Mirror mirrorLine)
         {
-            //Nothing inputted
+            //Null sapObj input
             if (sapObj == null)
             {
                 BH.Engine.Base.Compute.RecordError("No sap object inputted to mirror.");
                 return null;
             }
 
+            //If mirrorLine is null/None
+            if (mirrorLine == null || (mirrorLine == Mirror.None)) return sapObj;
+            
             BH.oM.Environment.SAP.XML.PropertyDetails propertyDetailsObj = sapObj.SAP10Data.PropertyDetails;
 
-            //Mirror the orientation of the dwelling
-            propertyDetailsObj.Orientation = propertyDetailsObj.Orientation.MirrorOrientation(mirrorLine);
-
-            //Try to mirror the PV in EnergySource
-            try
+            //Mirror the orientation of the dwelling if the orientation of the dwelling is not null
+            if (propertyDetailsObj.Orientation != null)
             {
+                string orientation = propertyDetailsObj.Orientation.MirrorOrientation(mirrorLine);
+
+                propertyDetailsObj.Orientation = orientation;
+            }
+            else
+            {
+                BH.Engine.Base.Compute.RecordError("Please input an orientation for the dwelling.");
+            }
+
+            //Mirror the PV in EnergySource
+            if (propertyDetailsObj.EnergySource != null && propertyDetailsObj.EnergySource.PhotovoltaicArrays != null && propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray != null)
+            {
+                //List of existing PVArrays in Energy source
                 List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> PVArrays = propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray;
-                foreach (var array in PVArrays)
-                {
-                    if (array != null)
-                    {
-                        if (array.Orientation == "0" || array.Orientation == "9")
-                        {
-                            BH.Engine.Base.Compute.RecordWarning("PV is horizontal and therefore can't be rotated");
-                            continue;
-                        }
-                        array.Orientation = array.Orientation.MirrorOrientation(mirrorLine);
-                    }
-                    else
-                    {
-                        continue;
-                    }
-                }
-                propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray = PVArrays;
-            }
-            catch (Exception e)
-            {
-                BH.Engine.Base.Compute.RecordWarning($"If you have no PV ignore this warning, otherwise: {e.Message}");
+
+                //Assign the list of modified arrays to the property details obj
+                propertyDetailsObj.EnergySource.PhotovoltaicArrays.PhotovoltaicArray = PVArrays.MirrorPV(mirrorLine);
             }
 
-            //Try to mirror the PV in MainHeating
-            try
+
+            //Mirror the PV in MainHeating
+            //Checking if Mainheating object is defined and if it isn't: side eye
+            if (propertyDetailsObj.Heating != null && propertyDetailsObj.Heating.MainHeatingDetails != null && propertyDetailsObj.Heating.MainHeatingDetails.MainHeating != null)
             {
+                //List of existing heating systems in report
                 List<MainHeating> heatingObjs = propertyDetailsObj.Heating.MainHeatingDetails.MainHeating;
+
+                //For each heating system
                 foreach (var heating in heatingObjs)
                 {
-                    List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> fghrsPVArrays = heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray;
-                    foreach (var array in fghrsPVArrays)
+                    //Check if pv is defined in the report
+                    if (heating.FGHRSEnergySource != null && heating.FGHRSEnergySource.PhotovoltaicArrays != null && heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray != null)
                     {
-                        if (array != null)
-                        {
-                            if (array.Orientation == "0" || array.Orientation == "9")
-                            {
-                                BH.Engine.Base.Compute.RecordWarning("PV is horizontal and therefore can't be rotated");
-                                continue;
-                            }
-                            array.Orientation = array.Orientation.MirrorOrientation(mirrorLine);
-                        }
-                        else
-                        {
-                            continue;
-                        }
+                        //A list of existing PV arrays in the Main Heating section
+                        List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> fghrsPVArrays = heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray;
+
+                        heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray = fghrsPVArrays.MirrorPV(mirrorLine);
                     }
-
-                    heating.FGHRSEnergySource.PhotovoltaicArrays.PhotovoltaicArray = fghrsPVArrays;
                 }
-
                 propertyDetailsObj.Heating.MainHeatingDetails.MainHeating = heatingObjs;
-
-            }
-            catch (Exception e)
-            {
-                BH.Engine.Base.Compute.RecordWarning($"If you have no PV ignore this warning, otherwise: {e.Message}");
             }
 
+            sapObj.SAP10Data.PropertyDetails = propertyDetailsObj.MirrorOpening(mirrorLine);
+
+            return sapObj;
+        }
+
+        [Description("Modify the orientation of the openings.")]
+        [Input("propertyDetailsObj", "PropertyDetails object to modify the openings within.")]
+        [Input("mirrorLine", "Line to mirror across.")]
+        [Output("propertyDetails", "Modified openings contained in the property details object.")]
+        public static BH.oM.Environment.SAP.XML.PropertyDetails MirrorOpening(this BH.oM.Environment.SAP.XML.PropertyDetails propertyDetailsObj, Mirror mirrorLine)
+        {
             //Mirror each opening in the dwelling
             List<BH.oM.Environment.SAP.XML.BuildingPart> buildingPartObj = propertyDetailsObj.BuildingParts.BuildingPart;
+            List<BH.oM.Environment.SAP.XML.OpeningType> types = propertyDetailsObj.OpeningTypes.OpeningType;
+
+            //Dictionary for the conversion between the name of the type given by the user and the name of the opening that matches the schema.
+            Dictionary<string, string> typeMap = types.Select(x => new { Key = x.Name, Value = x.Description }).ToDictionary(x => x.Key, x => x.Value);
+
             foreach (var b in buildingPartObj)
             {
+                //List of existing openings
                 List<BH.oM.Environment.SAP.XML.Opening> openingObjs = b.Openings.Opening;
                 foreach (var o in openingObjs)
                 {
                     if (o.Orientation == "0" || o.Orientation == "9")
                     {
-                        BH.Engine.Base.Compute.RecordWarning("Window is horizontal and therefore can't be mirrored");
                         continue;
                     }
-                    o.Orientation = o.Orientation.MirrorOrientation(mirrorLine);
+
+                    //Find the new orientation of the opening
+                    string orientation = o.Orientation.MirrorOrientation(mirrorLine);
+
+                    //Changing orientation
+                    o.Orientation = orientation;
                 }
                 b.Openings.Opening = openingObjs;
             }
 
             propertyDetailsObj.BuildingParts.BuildingPart = buildingPartObj;
-            sapObj.SAP10Data.PropertyDetails = propertyDetailsObj;
 
-            return sapObj;
+            return propertyDetailsObj;
+        }
+
+        [Description("Modify the orientation of the PV.")]
+        [Input("PVArrays", "List of photovoltaic arrays to modify the orientation of.")]
+        [Input("mirrorLine", "Line to mirror across.")]
+        [Output("PVarrayObject", "List of modified PV objects.")]
+        public static List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> MirrorPV(this List<BH.oM.Environment.SAP.XML.PhotovoltaicArray> PVArrays, Mirror mirrorLine)
+        {
+            //For each pv array
+            foreach (var array in PVArrays)
+            {
+                if (array != null)
+                {
+                    //If array orientation is ND - horizontal or NR not defined
+                    if (array.Orientation == "ND" || array.Orientation == "NR" || array.Orientation == null)
+                    {
+                        array.Orientation = "ND";
+                        continue;
+                    }
+
+                    //Find the mirrored orientation
+                    string orientation = array.Orientation.MirrorOrientation(mirrorLine);
+
+                    //Assign new orientation to array
+                    array.Orientation = orientation;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            return PVArrays;
         }
 
         [Description("Change compass direction by mirroring across common lines.")]
@@ -152,41 +190,28 @@ namespace BH.Engine.Environment.SAP
         [Output("Orientation", "New orientation.")]
         public static string MirrorOrientation(this string orientation, Mirror mirrorLine)
         {
-            if (orientation == null || orientation == "0")
+            //IF orientation is null or horizontal or not defined
+            if (orientation == null || orientation == "0" || orientation == "9" || orientation == "ND" || orientation == "NR")
             {
-                BH.Engine.Base.Compute.RecordError("No orientation to mirror.");
-                return null;
+                BH.Engine.Base.Compute.RecordWarning("Object does not have an orientation that can be mirrored.");
+                return orientation;
             }
 
-            List<int> compassPoints = new List<int>();
-            if (mirrorLine == Mirror.EastToWest)
-            {
-                compassPoints = new List<int>{3, 7};
-            }
-            else if (mirrorLine == Mirror.NorthWestToSouthEast)
-            {
-                compassPoints = new List<int>{4, 8};
-            }
-            else if (mirrorLine == Mirror.NorthToSouth)
-            {
-                compassPoints = new List<int> {1, 5};
-            }
-            else
-            {
-               compassPoints = new List<int> {2, 6};
-            };
+            List<int> compassPoints = new List<int> { (int)mirrorLine, (int)mirrorLine + 4 };
 
+            //Mirror the orientation over the selected line
             int orientationValue = Int32.Parse(orientation);
             int distance = compassPoints.Select(x => (x - orientationValue)).ToList().Min();
             int compassDirection = (orientationValue + 2 * (distance)) % 8;
 
             if (compassDirection <= 0)
             {
-                compassDirection = compassDirection + 8;
+                compassDirection += 8;
             }
-            
+
             return compassDirection.ToString();
         }
     }
+
 }
 
