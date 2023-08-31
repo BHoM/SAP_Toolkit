be598766b3b603c43b48241dce65922cb99839e6
Ellie Adebowale <ellie.adebowale@burohappold.com>
2023-08-31 17:31:25 +0100

    Commenting out files I don't want to change right now.

--- SAP_Engine/Modify/GroupByDwelling.cs
+++ SAP_Engine/Modify/GroupByDwelling.cs
@@ -1,104 +1,104 @@
-/*
- * This file is part of the Buildings and Habitats object Model (BHoM)
- * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
- *
- * Each contributor holds copyright over their respective contributions.
- * The project versioning (Git) records all such contribution source information.
- *                                           
- *                                                                              
- * The BHoM is free software: you can redistribute it and/or modify         
- * it under the terms of the GNU Lesser General Public License as published by  
- * the Free Software Foundation, either version 3.0 of the License, or          
- * (at your option) any later version.                                          
- *                                                                              
- * The BHoM is distributed in the hope that it will be useful,              
- * but WITHOUT ANY WARRANTY; without even the implied warranty of               
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
- * GNU Lesser General Public License for more details.                          
- *                                                                            
- * You should have received a copy of the GNU Lesser General Public License     
- * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
- */
+///*
+// * This file is part of the Buildings and Habitats object Model (BHoM)
+// * Copyright (c) 2015 - 2023, the respective contributors. All rights reserved.
+// *
+// * Each contributor holds copyright over their respective contributions.
+// * The project versioning (Git) records all such contribution source information.
+// *                                           
+// *                                                                              
+// * The BHoM is free software: you can redistribute it and/or modify         
+// * it under the terms of the GNU Lesser General Public License as published by  
+// * the Free Software Foundation, either version 3.0 of the License, or          
+// * (at your option) any later version.                                          
+// *                                                                              
+// * The BHoM is distributed in the hope that it will be useful,              
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+// * GNU Lesser General Public License for more details.                          
+// *                                                                            
+// * You should have received a copy of the GNU Lesser General Public License     
+// * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+// */
 
-//using BH.Engine.Base;
+////using BH.Engine.Base;
+////using BH.oM.Base;
+////using BH.oM.XML.Bluebeam;
+////using BH.oM.Environment.Elements;
+////using BH.oM.Geometry;
+////using BH.Engine.Geometry;
+////using BH.Engine.Units;
+////using BH.oM.Geometry.SettingOut;
+////using BH.oM.Analytical.Elements;
+////using System.Collections.ObjectModel;
+////using System.ComponentModel;
+////using BH.oM.Base.Attributes;
+////using System;
+////using System.Text;
+////using System.Threading.Tasks;
+
+////using BH.Engine.Data
+//using System.Collections.Generic;
+//using System.Linq;
+
+//using BH.oM.Environment.SAP;
 //using BH.oM.Base;
-//using BH.oM.XML.Bluebeam;
-//using BH.oM.Environment.Elements;
-//using BH.oM.Geometry;
-//using BH.Engine.Geometry;
-//using BH.Engine.Units;
-//using BH.oM.Geometry.SettingOut;
-//using BH.oM.Analytical.Elements;
-//using System.Collections.ObjectModel;
-//using System.ComponentModel;
+////using BH.Engine.Data.Query;
+//using Opening = BH.oM.Environment.SAP.Opening;
 //using BH.oM.Base.Attributes;
-//using System;
-//using System.Text;
-//using System.Threading.Tasks;
+//using System.ComponentModel;
 
-//using BH.Engine.Data
-using System.Collections.Generic;
-using System.Linq;
-
-using BH.oM.Environment.SAP;
-using BH.oM.Base;
-//using BH.Engine.Data.Query;
-using Opening = BH.oM.Environment.SAP.Opening;
-using BH.oM.Base.Attributes;
-using System.ComponentModel;
-
-namespace BH.Engine.Environment.SAP
-{
-    public static partial class Modify
-    {
-        [Description("Groups objects by dwelling.")]
-        [Input("openings", "List of openings.")]
-        [Input("walls", "List of walls.")]
-        [Input("floors", "List of floors.")]
-        [Input("livingAreas", "List of living areas.")]
-        [MultiOutput(0, "wallsList", "Lists of walls grouped by dwelling.")]
-        [MultiOutput(1, "openingsList", "Lists of openings grouped by dwelling.")]
-        [MultiOutput(2, "floorsList", "Lists of floors grouped by dwelling.")]
-        [MultiOutput(3, "livingAreaList", "Lists of living areas grouped by dwelling.")]
-        public static Output<List<List<Wall>>, List<List<Opening>>, List<List<Floor>>, List<List<LivingArea>>> GroupByDwelling (this List<Opening> openings, List<Wall> walls, List<Floor> floors, List<LivingArea> livingAreas)
-        {
-            //List<Opening> sortedOpenings = openings.OrderBy(x => x.DwellingName).ToList();
-            //List<Wall> sortedWalls = walls.OrderBy(x => x.DwellingName).ToList();
+//namespace BH.Engine.Environment.SAP
+//{
+//    public static partial class Modify
+//    {
+//        [Description("Groups objects by dwelling.")]
+//        [Input("openings", "List of openings.")]
+//        [Input("walls", "List of walls.")]
+//        [Input("floors", "List of floors.")]
+//        [Input("livingAreas", "List of living areas.")]
+//        [MultiOutput(0, "wallsList", "Lists of walls grouped by dwelling.")]
+//        [MultiOutput(1, "openingsList", "Lists of openings grouped by dwelling.")]
+//        [MultiOutput(2, "floorsList", "Lists of floors grouped by dwelling.")]
+//        [MultiOutput(3, "livingAreaList", "Lists of living areas grouped by dwelling.")]
+//        public static Output<List<List<Wall>>, List<List<Opening>>, List<List<Floor>>, List<List<LivingArea>>> GroupByDwelling (this List<Opening> openings, List<Wall> walls, List<Floor> floors, List<LivingArea> livingAreas)
+//        {
+//            //List<Opening> sortedOpenings = openings.OrderBy(x => x.DwellingName).ToList();
+//            //List<Wall> sortedWalls = walls.OrderBy(x => x.DwellingName).ToList();
 
            
-            List<List<Opening>> groupedOpenings = openings.OrderBy(x => x.DwellingName)
-                                                          .ToList()
-                                                          .GroupBy(x => x.DwellingName)
-                                                          .Select(grp => grp.ToList())
-                                                          .ToList();
+//            List<List<Opening>> groupedOpenings = openings.OrderBy(x => x.DwellingName)
+//                                                          .ToList()
+//                                                          .GroupBy(x => x.DwellingName)
+//                                                          .Select(grp => grp.ToList())
+//                                                          .ToList();
 
-            List<List<Wall>> groupedWalls = walls.OrderBy(x => x.DwellingName)
-                                                          .ToList()
-                                                          .GroupBy(x => x.DwellingName)
-                                                          .Select(grp => grp.ToList())
-                                                          .ToList();
+//            List<List<Wall>> groupedWalls = walls.OrderBy(x => x.DwellingName)
+//                                                          .ToList()
+//                                                          .GroupBy(x => x.DwellingName)
+//                                                          .Select(grp => grp.ToList())
+//                                                          .ToList();
 
-            List<List<Floor>> groupedTFA = floors.OrderBy(x => x.DwellingName)
-                                                          .ToList()
-                                                          .GroupBy(x => x.DwellingName)
-                                                          .Select(grp => grp.ToList())
-                                                          .ToList();
+//            List<List<Floor>> groupedTFA = floors.OrderBy(x => x.DwellingName)
+//                                                          .ToList()
+//                                                          .GroupBy(x => x.DwellingName)
+//                                                          .Select(grp => grp.ToList())
+//                                                          .ToList();
 
-            List<List<LivingArea>> groupedLivingAreas = livingAreas.OrderBy(x => x.DwellingName)
-                                                          .ToList()
-                                                          .GroupBy(x => x.DwellingName)
-                                                          .Select(grp => grp.ToList())
-                                                          .ToList();
+//            List<List<LivingArea>> groupedLivingAreas = livingAreas.OrderBy(x => x.DwellingName)
+//                                                          .ToList()
+//                                                          .GroupBy(x => x.DwellingName)
+//                                                          .Select(grp => grp.ToList())
+//                                                          .ToList();
 
-            return new Output<List<List<Wall>>, List<List<Opening>>, List<List<Floor>>, List<List<LivingArea>>>()
-            {
-                Item1 = groupedWalls,
-                Item2 = groupedOpenings,
-                Item3 = groupedTFA,
-                Item4 = groupedLivingAreas
-            };
-        }
-    }
-}
+//            return new Output<List<List<Wall>>, List<List<Opening>>, List<List<Floor>>, List<List<LivingArea>>>()
+//            {
+//                Item1 = groupedWalls,
+//                Item2 = groupedOpenings,
+//                Item3 = groupedTFA,
+//                Item4 = groupedLivingAreas
+//            };
+//        }
+//    }
+//}
 
 
