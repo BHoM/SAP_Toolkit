c8760dc61f5b5f6a44a8a976d89f24b8adc48e96
Fraser Greenroyd <Fraser.Greenroyd@burohappold.com>
2023-09-04 17:56:20 +0100

    Start on compiling SAPReport

---
+++ SAP_Adapter/CRUD/SAPReport.cs
@@ -0,0 +1,31 @@
+﻿using BH.Engine.Adapter;
+using BH.oM.Base;
+using BH.oM.Environment.SAP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using BH.oM.Adapters.Excel;
+using BH.Adapter.Excel;
+using System.Linq;
+using BH.oM.Environment.SAP.Excel;
+using BH.oM.Environment.SAP.XML;
+
+namespace BH.Adapter.SAP
+{
+    public partial class SAPAdapter
+    {
+        private List<SAPReport> ReadSAPReport(SAPConfig config)
+        {
+            var sapMarkupSummary = ReadSAPMarkupSummary(config)?[0];
+            if(sapMarkupSummary == null)
+            {
+                BH.Engine.Base.Compute.RecordError("Mark Up Summary did not return a viable object to produce a SAP Report with.");
+                return new List<SAPReport>();
+            }
+
+            //Openings
+            var openingSummaries = sapMarkupSummary.Markup.Where(x => x.Layer == "Openings").ToList();
+        }
+    }
+}
