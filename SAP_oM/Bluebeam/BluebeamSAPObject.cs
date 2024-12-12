4fdd00a0e88bc8fc4b5b4e152416cf41e324b980
Arnaud Declercq <arnaud.declercq@burohappold.com>
2024-12-12 13:40:39 +0000

    Add default value to core properties

--- SAP_oM/Bluebeam/BluebeamSAPObject.cs
+++ SAP_oM/Bluebeam/BluebeamSAPObject.cs
@@ -35,13 +35,14 @@ namespace BH.oM.Environment.SAP.Bluebeam
         [XmlIgnore]
         public virtual Guid BHoM_Guid { get; set; } = Guid.NewGuid();
         [XmlIgnore]
-        public virtual Dictionary<string, object> CustomData { get; set; }
+        public virtual Dictionary<string, object> CustomData { get; set; } = new Dictionary<string, object>();
         [XmlIgnore]
-        public virtual string Name { get; set; }
+        public virtual string Name { get; set; } = "";
         [XmlIgnore]
-        public virtual FragmentSet Fragments { get; set; }
+        public virtual FragmentSet Fragments { get; set; } = new FragmentSet();
         [XmlIgnore]
-        public virtual HashSet<string> Tags { get; set; }
+        public virtual HashSet<string> Tags { get; set; } = new HashSet<string>();
+
     }
 }
 
